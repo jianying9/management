b2a3f20053457b6c8d9ae02b2ee481d7fa186c6a
jianying9 <jianying9@192.168.1.122>
2017-04-12 14:32:31 +0800

    1、增加通信业务协议文档 2、测试服务时自动生成请求的json

--- src/main/webapp/js/controllers/DashboardController.js
+++ src/main/webapp/js/controllers/DashboardController.js
@@ -11,4 +11,54 @@ WolfApp.controller('DashboardController', function ($scope, $rootScope) {
         tIndex = index % btnType.length;
         $scope.projects[index].btnType = btnType[tIndex];
     }
+    var requestJson = {
+        route: '/app/route',
+        callback: '请求唯一标识,jsonp中也是回调函数名(非必要)',
+        md5: '数据变化校验(非必要)',
+        param: {
+            param1: "业务接口传参数位置,可以是任意的json,具体看每个接口定义",
+            param2: 100,
+            param3: true,
+            param4: {},
+            param5: []
+        }
+    };
+    $scope.requestJson = angular.toJson(requestJson, 4);
+    //
+    var httpRequestJson = {
+        param1: "业务接口传参数位置,可以是任意的json,具体看每个接口定义",
+        param2: 100,
+        param3: true,
+        param4: {},
+        param5: []
+    };
+    $scope.httpRequestJson = angular.toJson(httpRequestJson, 4);
+    //
+    var responseJson = {
+        route: '/app/route',
+        code: '返回操作结果标识:unlogin-未登录,invalid-非法参数,timeout-session过期,unmoifyed-数据没有变化,notfound-route对应的服务不存在',
+        callback: '如果请求有callback,那么响应就会返回请求标识,jsonp中也会用到',
+        md5: '如果请求参数有md5,响应的数据如果有新的变化,则会返回最新的md5值',
+        data: {
+            param1: "返回业务数据,可以是任意的json,具体看每个接口的定义",
+            param2: 100,
+            param3: true,
+            param4: {},
+            param5: []
+        }
+    };
+    $scope.responseJson = angular.toJson(responseJson, 4);
+    //
+    var pushJson = {
+        route: '/push/route',
+        pushId: '推送id',
+        data: {
+            param1: "返回业务数据,可以是任意的json,具体看每个接口的定义",
+            param2: 100,
+            param3: true,
+            param4: {},
+            param5: []
+        }
+    };
+    $scope.pushJson = angular.toJson(pushJson, 4);
 });
--- src/main/webapp/js/controllers/ServiceController.js
+++ src/main/webapp/js/controllers/ServiceController.js
@@ -10,6 +10,7 @@ WolfApp.controller('ServiceController', function ($stateParams, $scope, $cookies
     $scope.pushArray = [];
     $scope.testInfo = '';
     $scope.testJson = "{}";
+    $scope.testJsonLine = 6;
     if ($cookies[$scope.routeName]) {
         $scope.testJson = $cookies[$scope.routeName];
     }
@@ -33,6 +34,87 @@ WolfApp.controller('ServiceController', function ($stateParams, $scope, $cookies
             push.route = push.routeName.replace(/\//g, '-');
         });
         $scope.requests = obj.requestArray;
+        $scope.testJsonLineNum = obj.requestArray.length + 2;
+        var testParam = {};
+        var getChildParam = function (parentName, requests) {
+            var result = {};
+            angular.forEach(requests, function (request) {
+                if (request.name.indexOf(parentName) === 0) {
+                    var childName = request.name.substring(parentName.length + 1);
+                    if (childName.indexOf('.') === -1 && childName.length > 0) {
+                        switch (request.type) {
+                            case 'OBJECT':
+                                result[childName] = createChildParam(request, requests);
+                                break;
+                            case 'OBJECT_ARRAY':
+                                result[childName] = createChildParam(request, requests);
+                                break;
+                            case 'STRING_ARRAY':
+                                $scope.testJsonLineNum += 2;
+                                result[childName] = [''];
+                                break;
+                            case 'LONG_ARRAY':
+                                $scope.testJsonLineNum += 2;
+                                result[childName] = [-1];
+                                break;
+                            case 'BOOLEAN':
+                                result[childName] = true;
+                                break;
+                            case 'LONG':
+                                result[childName] = -1;
+                                break;
+                            default:
+                                result[childName] = '';
+                        }
+                        $scope.$apply();
+                    }
+                }
+            });
+            return result;
+        };
+        var createChildParam = function (parentRequest, requests) {
+            $scope.testJsonLineNum += 2;
+            var result;
+            if (parentRequest.type === 'OBJECT') {
+                result = getChildParam(parentRequest.name, requests);
+            } else if (parentRequest.type === 'OBJECT_ARRAY') {
+                var child = getChildParam(parentRequest.name, requests);
+                result = [];
+                result.push(child);
+            }
+            return result;
+        };
+        angular.forEach(obj.requestArray, function (request) {
+            var paramName = request.name;
+            if (paramName.indexOf('.') === -1 && paramName.length > 0) {
+                switch (request.type) {
+                    case 'OBJECT':
+                        testParam[paramName] = createChildParam(request, obj.requestArray);
+                        break;
+                    case 'OBJECT_ARRAY':
+                        testParam[paramName] = createChildParam(request, obj.requestArray);
+                        break;
+                    case 'STRING_ARRAY':
+                        $scope.testJsonLineNum += 2;
+                        testParam[paramName] = [''];
+                        break;
+                    case 'LONG_ARRAY':
+                        $scope.testJsonLineNum += 2;
+                        testParam[paramName] = [-1];
+                        break;
+                    case 'BOOLEAN':
+                        testParam[paramName] = true;
+                        break;
+                    case 'LONG':
+                        testParam[paramName] = -1;
+                        break;
+                    default:
+                        testParam[paramName] = '';
+                }
+                $scope.$apply();
+            }
+        });
+        $scope.testJson = angular.toJson(testParam, 4);
         $scope.responseCodes = obj.codeArray;
         $scope.responses = obj.responseArray;
         $scope.pushArray = obj.pushArray;
